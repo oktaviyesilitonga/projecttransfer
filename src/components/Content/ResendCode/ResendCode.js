6af0135d2763294cae4d59c19792f817d6f0f724
oktavi <oktavisilitonga14@gmail.com>
2019-01-18 15:24:07 +1100

    updated some components

--- src/components/Content/ResendCode/ResendCode.js
+++ src/components/Content/ResendCode/ResendCode.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import EnterOtp from '../../../assets/images/icon/enter-otp.svg';
 import ErrorOtp from '../../../assets/images/icon/error-otp.svg';
+import { Link } from 'react-router-dom';
 
 class ResendCode extends Component {
     render() {
         return (
             <div>
-                <div class="resend-code-icon">
-                    <img src={EnterOtp} alt="Enter OTP" class="icon-initialize" />
-                    <img src={ErrorOtp} alt="Enter OTP" class="icon-error-otp" />
+                <div className="resend-code-icon">
+                    <img src={EnterOtp} alt="Enter OTP" className="icon-initialize" />
+                    <img src={ErrorOtp} alt="Enter OTP" className="icon-error-otp" />
                 </div>
-                <div class="resend-code-title">
+                <div className="resend-code-title">
                     <h5><span>6 Digit</span> Kode Verifikasi</h5>
                 </div>
-                <div class="resend-code-error-otp">
+                <div className="resend-code-error-otp">
                     Maaf, kode yang kamu masukkan salah.<br />
                     Mohon coba kembali.
 				</div>
-                <div class="resend-code-form">
-                    <form action="detail-bank.html" class="form jsFormOTPCode" autocomplete="off">
-                        <div class="input-row">
-                            <input name="otp_code" type="text" class="input-text jsInputNumber jsOTPCode" pattern="\d*" maxlength="6" value="" autocomplete="off" />
+                <div className="resend-code-form">
+                    <form action="detail-bank.html" className="form jsFormOTPCode" autocomplete="off">
+                        <div className="input-row">
+                            <input name="otp_code" type="text" clasName="input-text jsInputNumber jsOTPCode" pattern="\d*" maxlength="6" value="" autocomplete="off" />
                         </div>
                     </form>
                 </div>
-                <div class="resend-code-button">
-                    <a href="resend-code.html" class="resend-button">Kirim Ulang Kode</a>
-                    <a href="welcome.html" class="close-button">Tombol Tutup</a>
+                <div className="resend-code-button">
+                    <Link to="/resendcode" className="resend-button">Kirim Ulang Kode</Link>
+                    <Link to="/" className="close-button">Tombol Tutup</Link>
                 </div>
-                <div class="wrapper-button">
-                    <button type="button" class="button button-primary button-disabled jsConfirmVerificationCode" data-jsonurl="data/resend-code.json">Konfirmasi Kode</button>
+                <div className="wrapper-button">
+                    <button type="button" className="button button-primary button-disabled jsConfirmVerificationCode" data-jsonurl="data/resend-code.json">Konfirmasi Kode</button>
                 </div>
             </div>
         )
--- src/components/Content/Welcome/Welcome.js
+++ src/components/Content/Welcome/Welcome.js
@@ -5,9 +5,9 @@ class Welcome extends Component {
     render() {
         return (
             <div>
-                <p>Halo, Selamat datang di <strong class="color-primary">PermataBank</strong></p>
+                <p>Halo, Selamat datang di <strong className="color-primary">PermataBank</strong></p>
                 <p>Silakan menekan tombol <strong>Lanjut</strong> untuk menyelesaikan Transfer</p>
-                <div class="wrapper-button">
+                <div className="wrapper-button">
                     <Link to="/resendcode" class="button button-primary">Lanjut</Link>
                 </div>
             </div>
